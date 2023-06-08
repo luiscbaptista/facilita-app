2b83d39c1428fd465c307c95740a4181cd5b8223
luiscbaptista <luiscbaptista0@gmail.com>
2023-06-08 12:57:16 +0100

    C

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,13 +6,7 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
 `
+
 export const Main = styled.div`
   min-height: 500px;
-`
-
-export const TextInput = styled.input`
-  padding: 1rem;
-  border-radius: .5rem;
-  width: 400px;
-  border: none;
 `
